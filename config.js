6d9c06f22fdb748402ef3cf7e06e6a46b6766791
404Kurama <kurama.notfound@gmail.com>
2023-02-12 15:53:37 +0700

    feat(config): update

--- config.js
+++ config.js
@@ -6,82 +6,80 @@
 // └─┘└─┘┘└┘└  ┴└─┘└─┘┴└─┴ ┴ ┴ ┴└─┘┘└┘
 
 const CONFIG = {
-	// ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
-	// ├┴┐├─┤└─┐││  └─┐
-	// └─┘┴ ┴└─┘┴└─┘└─┘
+  // ┌┐ ┌─┐┌─┐┬┌─┐┌─┐
+  // ├┴┐├─┤└─┐││  └─┐
+  // └─┘┴ ┴└─┘┴└─┘└─┘
 
-	// General
-	name: 'Rei!',
-	openInNewTab: false,
-	twelveHourFormat: true,
+  // General
+  name: "Kurama",
+  openInNewTab: false,
+  twelveHourFormat: true,
 
-	// Theme
+  // Theme
 
-    theme: 'latte-mocha',
-	imageBackground: false,
-	
-	// Greetings
-	greetingMorning: 'Good morning, ',
-	greetingAfternoon: 'Good afternoon,',
-	greetingEvening: 'Good evening,',
-	greetingNight: 'Good Night, ',
+  theme: "latte-mocha",
+  imageBackground: false,
 
-	// Layout
-	bentoLayout: 'buttons', // 'bento', 'lists', 'buttons'
+  // Greetings
+  greetingMorning: "Good morning, ",
+  greetingAfternoon: "Good afternoon,",
+  greetingEvening: "Good evening,",
+  greetingNight: "Good Night, ",
 
-	// Autochange
-	autoChangeTheme: false,
+  // Layout
+  bentoLayout: "buttons", // 'bento', 'lists', 'buttons'
 
-	// Autochange by OS
-	changeThemeByOS: false,
+  // Autochange
+  autoChangeTheme: false,
 
-	// Autochange by hour options (24hrs format, string must be in: hh:mm)
-	changeThemeByHour: false,
-	hourDarkThemeActive: '18:30',
-	hourDarkThemeInactive: '07:00',
+  // Autochange by OS
+  changeThemeByOS: false,
 
-	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
-	// ├┴┐│ │ │  │ │ ││││└─┐
-	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+  // Autochange by hour options (24hrs format, string must be in: hh:mm)
+  changeThemeByHour: false,
+  hourDarkThemeActive: "18:30",
+  hourDarkThemeInactive: "07:00",
 
-	firstButtonsContainer: [
-		{
-			id: '1',
-			name: 'Github',
-			icon: 'github',
-			link: 'https://github.com/',
-		},
-		{
-			id: '2',
-			name: 'Mail',
-			icon: 'mail',
-			link: 'https://mail.google.com/',
-		},
-		{
-			id: '3',
-			name: 'Reddit',
-			icon: 'glasses',
-			link: 'https://reddit.com',
-		},
+  // ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+  // ├┴┐│ │ │  │ │ ││││└─┐
+  // └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
 
-		{
-			id: '4',
-			name: 'Music',
-			icon: 'headphones',
-			link: 'https://music.youtube.com',
-            
-		},
-		{
-			id: '5',
-            name: 'Youtube',
-            icon: 'youtube',
-            link: 'https://youtube.com'
-		},
-		{
-			id: '6',
-			name: 'Discord',
-			icon: 'bot',
-			link: 'https://discord.com/app',
-		},
-	],
+  firstButtonsContainer: [
+    {
+      id: "1",
+      name: "Github",
+      icon: "github",
+      link: "https://github.com/",
+    },
+    {
+      id: "2",
+      name: "Mail",
+      icon: "mail",
+      link: "https://mail.google.com/",
+    },
+    {
+      id: "5",
+      name: "Reddit",
+      icon: "glasses",
+      link: "https://reddit.com",
+    },
+    {
+      id: "3",
+      name: "Youtube",
+      icon: "youtube",
+      link: "https://youtube.com",
+    },
+    {
+      id: "4",
+      name: "Calendar",
+      icon: "calendar",
+      link: "https://calendar.google.com/calendar/r/",
+    },
+    {
+      id: "6",
+      name: "Music",
+      icon: "headphones",
+      link: "https://www.spotify.com",
+    },
+  ],
 };
